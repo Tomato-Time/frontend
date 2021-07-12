16807f3664af6a11e40aa5d30ead724acf19328b
Nicholas de Souza <nrdesouzag13@gmail.com>
2021-07-12 18:47:49 -0400

    got text fields

---
+++ ui/src/LoginPage/loginButton.js
@@ -0,0 +1,18 @@
+import Button from '@material-ui/core/Button';
+import './loginButton.css'
+
+export default function LoginButton(){
+
+   
+
+
+    return(
+        <div className="loginButton">
+
+            <Button className="btn" variant="contained" color="primary">
+                Log in
+            </Button>
+
+        </div>
+    )
+}
--- ui/src/index.js
+++ ui/src/index.js
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+     primary: {
+        main: "#FFFFFF" // This is an orange looking color
+               },
+     secondary: {
+        main: "#18A0FB" //Another orange-ish color
+                }
+           },
+  root:{
+    backgroundColor: '#fff'
+  }         
+});
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}><App /></ThemeProvider>,
   document.getElementById('root')
 );
 
