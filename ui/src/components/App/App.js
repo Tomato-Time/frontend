ed7392c09f99ad496877f34ab1b6e09f221cc59d
Yuvia Leon <yuvy.leon.yl@gmail.com>
2021-07-19 11:19:20 -0500

    changed structure of files

---
+++ ui/src/components/App/App.js
@@ -0,0 +1,14 @@
+import SignInSide from "../LoginPage/loginForm";
+import Timer from "../Timer/timer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+      {/* <SignInSide/> */}
+    </div>
+  );
+}
+
+export default App;
