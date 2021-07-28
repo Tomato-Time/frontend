d0f458d8a6d9fc9906e2c5f2086110b345f44035
Nicholas de Souza <nrdesouzag13@gmail.com>
2021-07-28 11:45:23 -0400

    wrong branch but now the stepper tracks timer rounds

--- ui/src/Components/HomePage/homePage.js
+++ ui/src/Components/HomePage/homePage.js
@@ -1,14 +1,22 @@
 import Timer from "../Timer/timer";
 import MiniDrawer from "../SideBar/sidebar";
 import HorizontalLabelPositionBelowStepper from "../TimerProgress/timerProgress";
+import { RoundContext } from "../../RoundContext";
+import { useState } from "react";
 
 export default function HomePage(){
+
+
+    const [round, setRound] = useState(0);
     //just to render the components for the home page
     return(
         <div>
-            <Timer/>
-            <MiniDrawer/>
-            <HorizontalLabelPositionBelowStepper/>
+
+            <RoundContext.Provider value={{round, setRound}}>
+                <Timer/>
+                <MiniDrawer/>
+                <HorizontalLabelPositionBelowStepper/>
+            </RoundContext.Provider>
         </div>
     )
 }
